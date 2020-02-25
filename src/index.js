69c4c6c0aba34aaf50fb102d3b7f0064084a665f
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-25 13:19:06 +0300

    init: start  algorithmic-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number){
+    number = String(number);
+    let arr = [];
+    for (i = 0; i< number.length; i++) {
+        arr.unshift(number[i]);
+    }
+   return arr =Number( arr.join(""));
 }
